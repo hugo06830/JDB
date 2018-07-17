22d755e2a466fbada5c3c6b07a56a38a98463f85
hugo06830 <40829656+hugo06830@users.noreply.github.com>
2018-07-17 15:16:15 +0200

    Version 1.1 colors added

--- Poc-Hologram/src/fr/hugo06830/hologram/commandes/CommandHologram.java
+++ Poc-Hologram/src/fr/hugo06830/hologram/commandes/CommandHologram.java
@@ -1,7 +1,9 @@
 package fr.hugo06830.hologram.commandes;
 
+
+
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,28 +11,66 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
+
+
 public class CommandHologram
   implements CommandExecutor
 {
+	
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
   {
-    if (((sender instanceof Player)) && (cmd.getName().equalsIgnoreCase("hologram")) && (sender.isOp()))
-    {
+    if (((sender instanceof Player)) && (cmd.getName().equalsIgnoreCase("hologram")) && (sender.isOp())){
+    
+    	
       Player player = (Player)sender;
-      Location ploc = player.getLocation();
+      Location ploc =  player.getLocation();
+      
+      	if(args.length ==0) {
+      		player.sendMessage(ChatColor.RED + "la commande est : /hologram  <message>");
+      	}else {
+      	
       StringBuilder bc = new StringBuilder();
       String[] arrayOfString;
       int j = (arrayOfString = args).length;
       for (int i = 0; i < j; i++)
       {
         String parts = arrayOfString[i];
+       if(parts.contentEquals("RED")) {
+    	   parts = "§c";
+       }
+       if(parts.contentEquals("GREEN")) {
+    	   parts = "§a";
+       }
+   
+       if(parts.contentEquals("YELLOW")) {
+    	   parts ="§e";
+       }
+   
+       if(parts.contentEquals("BLUE")) {
+    	   parts = "§9";
+       }
+      
+   
         bc.append(" " + parts);
       }
+      
+      
       ArmorStand stand = (ArmorStand)ploc.getWorld().spawnEntity(ploc, EntityType.ARMOR_STAND);
       stand.setVisible(false);
       stand.setCustomName(bc.toString());
       stand.setCustomNameVisible(true);
+      stand.setGravity(false);
+            
+
+      
+      }
+  
+    	
+    	
+  return true;  
+    	
     }
+    
     return false;
   }
 }
